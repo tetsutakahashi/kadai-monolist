575d3bf1fb4fe3dac03563d346767ae8cc42af46
EC2 Default User <ec2-user@ip-172-31-35-234.us-east-2.compute.internal>
2018-08-05 02:23:17 +0000

    rankingwant

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use \App\Item;
 
+use App\Http\Controllers\Controller;
+
 class ItemsController extends Controller
 {
 
--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,9 @@ Route::group(['middleware' => ['auth']], function () {
     Route::resource('items', 'ItemsController', ['only' => ['create']]);
 });
 
+// ランキング
+Route::get('ranking/want', 'RankingController@want')->name('ranking.want');
+
 Route::group(['middleware' => ['auth']], function () {
     Route::resource('items', 'ItemsController', ['only' => ['create', 'show']]);
     Route::post('want', 'ItemUserController@want')->name('item_user.want');
